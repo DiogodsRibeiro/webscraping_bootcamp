6d283516f7c6b218ec103c45eb1fdb5016b15d35
Diogo Souza <diogodsribeiro@gmail.com>
2025-04-18 21:22:55 -0300

    Finalizando

--- src/get_my_ip.py
+++ src/get_my_ip.py
@@ -3,11 +3,10 @@ import requests
 url = "https://lumtest.com/myip.json"
 url2 = "https://www.globo.com"
 
-response = requests.get(url2)
+response = requests.get(url)
 
 if response.status_code == 200:
     print(response.json())
 
 else:
     print("Erro")
-
